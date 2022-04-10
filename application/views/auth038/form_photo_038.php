eefa17d24362bc48a944ad037ec820c957984c85
zulzunman <101050815+zulzunman@users.noreply.github.com>
2022-04-11 06:54:31 +0700

    fix3css

--- application/views/auth038/form_photo_038.php
+++ application/views/auth038/form_photo_038.php
@@ -4,28 +4,58 @@
 <head>
     <meta charset="UTF-8">
     <title>CATSHOP 038</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="<?php echo base_url();?>/asset/plugins/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/asset/plugins/bootstrap/css/bootstrap-slider.css">
+    <!-- Font Awesome -->
+    <link href="<?php echo base_url();?>/asset/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 </head>
-<body>
-    <h1>CATSHOP 038</h1>
-    <h3>CHANGE PHOTO</h3>
-    <hr>
-    <?=$this->session->flashdata('msg')?>
-    <div style="color:red;"><?=$error?></div>
-    <form action="" method="post" enctype="multipart/form-data"> 
-        <table>
-            <tr>
-                <td>CURRENT PHOTO</td>
-                <td><img src="<?=base_url('uploads/users/'.$this->session->userdata('photo'))?>" width="128" height="128"></td>
-            </tr>
-            <tr>
-                <td>NEW PHOTO</td>
-                <td><input type="file" name="photo" required></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><input type="submit" name="upload" value="UPLOAD PHOTO"></td>
-            </tr>
-        </table>
-    </form><hr><a href="<?=base_url()?>"><h4>Back to Home</h4></a>
+<body class="p-2 mb-1 bg-secondary" class="bg-gradient">
+    <div class="main main-raised">
+        <div class="profile-content">
+            <div class="container p-4 my-4 bg-light rounded">
+                <form action="" method="post" enctype="multipart/form-data">
+                    <div class="card-header text-center">
+                    <div class="row">
+                        <div class="col-md-6 ml-auto mr-auto">
+                            <div class="profile">
+                                <div class="avatar">
+                                    <img src="<?=base_url('uploads/users/'.$this->session->userdata('photo'))?>" width="128" height="128" class="img-raised rounded-circle img-fluid">
+                                </div>
+                                <div class="name">
+                                    <h3 class="title">CURRENT PHOTO</h3>
+                                    <h6><?=$this->session->userdata('usertype')?></h6>
+                                </div>
+                                <div class="card-title text-center"> <?=$this->session->flashdata('msg')?></div>
+                            </div>
+                            <div style="color: red;"><?=$error?></div>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="col-md-6 ml-auto mr-auto">
+                            <p>NEW PHOTO</p>
+                            <input type="file" name="photo" required>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 ml-auto mr-auto">
+                            <input type="submit" name="upload" value="UPLOAD PHOTO">
+                        </div>
+                    </div>
+                    </div>
+                </form><br>
+                <a href="<?=base_url()?>" class="btn btn-primary">Back to Home</a>
+            </div>
+        </div>
+    </div><br><br>
+        <footer class="footer footer-default">
+            <div class="container p-2 my-2 bg-blue-900">
+            <div class="copyright float-right">
+                made with <i class="fa fa-dragon" style="color:red;"> ZUNN</i>
+            </div>
+            </div>
+        </footer>
 </body>
 </html>
--- application/views/cats038/sale_list038.php
+++ application/views/cats038/sale_list038.php
@@ -3,31 +3,55 @@
 <html lang="en">
 <head>
     <title>CATSHOP 038</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="<?php echo base_url();?>/asset/plugins/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/asset/plugins/bootstrap/css/bootstrap-slider.css">
+    <!-- Font Awesome -->
+    <link href="<?php echo base_url();?>/asset/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 </head>
-<body>
-    <h1>CATSHOP 038</h1>
-    <h3>SALE LIST</h3>
-    <a href="<?=base_url()?>"><h4>HOME</h4></a><hr>
-    <table border="1px solid black">
-        <tr>
-            <th>No</th>
-            <th>Sale ID</th>
-            <th>Sale Data</th>
-            <th>Cat ID</th>
-            <th>Customer Name</th>
-            <th>Customer Address</th>
-            <th>Customer Phone</th>
-        </tr>
-        <?php $i=1; foreach($sales as $sale) { ?>
-        <tr>
-            <td><?=$i++?></td>
-            <td><?=$sale->sale_id_038?></td>
-            <td><?=$sale->sale_date_038?></td>
-            <td><?=$sale->cat_id_038?></td>
-            <td><?=$sale->customer_name_038?></td>
-            <td><?=$sale->customer_address_038?></td>
-            <td><?=$sale->customer_phone_038?></td>
-        </tr> <?php } ?>
-    </table>
+<body class="p-2 mb-1 bg-secondary" class="bg-gradient">
+    <i class="fa fa-shopping-bag" style="font-size: 48px;color:red;"> </i><i class="fa fa-shopping-bag" style="font-size: 48px;color:red;"> </i>
+    <i class="fa fa-shopping-bag" style="font-size: 48px;color:red;float: right;"> </i><i class="fa fa-shopping-bag" style="font-size: 48px;color:red;float: right;"> </i>
+    <div class="main main-raised">
+        <div class="profile-content">
+            <div class="container p-2 my-3 bg-light rounded">
+                <div class="card-header text-center">
+                    <h3 class="card-title">SALE LIST</h3>
+                </div>
+                <div>
+                <a href="<?=base_url()?>" class="btn btn-primary">HOME</a>
+                </div><br>
+                <table border="2px solid black" align="center" class="table table-info table-striped p-3 my-1">
+                    <tr>
+                        <th>No</th>
+                        <th>Sale ID</th>
+                        <th>Sale Data</th>
+                        <th>Cat ID</th>
+                        <th>Customer Name</th>
+                        <th>Customer Address</th>
+                        <th>Customer Phone</th>
+                    </tr>
+                    <?php $i=1; foreach($sales as $sale) { ?>
+                    <tr>
+                        <td><?=$i++?></td>
+                        <td><?=$sale->sale_id_038?></td>
+                        <td><?=$sale->sale_date_038?></td>
+                        <td><?=$sale->cat_id_038?></td>
+                        <td><?=$sale->customer_name_038?></td>
+                        <td><?=$sale->customer_address_038?></td>
+                        <td><?=$sale->customer_phone_038?></td>
+                    </tr> <?php } ?>
+                </table>
+            </div>
+        </div>
+    </div><br><br>
+        <footer class="footer footer-default">
+            <div class="container p-2 my-2 bg-blue-900">
+            <div class="copyright float-right">
+                made with <i class="fa fa-dragon" style="color:blue;"> ZUNN</i>
+            </div>
+            </div>
+        </footer>
 </body>
 </html>
